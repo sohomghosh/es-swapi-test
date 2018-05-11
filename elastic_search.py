885fcd10b134031d27d7715d3190360b3b1819ce
Sohom Ghosh <sohom1ghosh@gmail.com>
2018-05-11 20:55:17 +0530

    Update elastic_search.py

--- elastic_search.py
+++ elastic_search.py
@@ -1,5 +1,4 @@
 #group by, aggregate operator
-
 res= es.search(index='megacorp',doc_type='employee',body={
         "aggs": {
             "all_interests": {
@@ -7,3 +6,7 @@ res= es.search(index='megacorp',doc_type='employee',body={
             }
         }
     })
+
+
+#other elastic search examples
+#https://qbox.io/blog/python-scripts-interact-elasticsearch-examples
